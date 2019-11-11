4ccd2cbe11c5d6f5ec7f61c935c949462c4c2ea0
jz0001 <jinming.zhang.92@gmail.com>
2019-11-11 17:26:23 -0500

    able to link the file containing tiles array

--- memery_frontend/src/index.js
+++ memery_frontend/src/index.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
     let usersURL = "http://localhost:3000/users"
     let tilesURL = "http://localhost:3000/tiles"
 
-    // var tilesArray = require("./tiles").tilesArray
     console.log(tilesArray, tilesBack)
 
   })//end of DOM Loading
